23e80f48c31509d820b6bd2f264111ddf7f37b6f
Ajay Yadav <ajay.yadav@tothenew.com>
2021-11-05 17:43:06 +0530

    Initial commit

---
+++ src/main/java/com/ighmcleod/IghmcleodApplication.java
@@ -0,0 +1,13 @@
+package com.ighmcleod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IghmcleodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IghmcleodApplication.class, args);
+	}
+
+}
